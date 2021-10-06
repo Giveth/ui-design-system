1a2b04a456a9e1ebf21f6c128a15ff8695f9e54c
cherik <pourcheriki@gmail.com>
2021-10-06 12:23:19 +0330

    add subline stories

---
+++ src/stories/typography/body/Subline.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Subline } from '../../../components/typography/body/Subline';
+
+export default {
+  title: 'Example/Subline',
+  component: Subline,
+} as ComponentMeta<typeof Subline>;
+
+const Template: ComponentStory<typeof Subline> = (args) => <Subline {...args}>{args.label || 'Was he a beast if music could move him so?'}</Subline>;
+
+
+export const Example = Template.bind({});
