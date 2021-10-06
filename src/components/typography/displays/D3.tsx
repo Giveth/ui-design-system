3396abc177f484a2c55c490188dc15faf1c5f523
cherik <pourcheriki@gmail.com>
2021-10-06 15:15:47 +0330

    add D3

---
+++ src/components/typography/displays/D3.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { brandColors } from "../../../common/colors";
+
+export const D3 = styled.div`
+    /* Display/D3 */
+    font-family: TeX Gyre Adventor;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 88px;
+    line-height: 120%;
+    letter-spacing: -0.04em;
+    color: ${brandColors.deep[900]};
+`;
