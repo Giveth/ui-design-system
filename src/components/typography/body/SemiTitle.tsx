0f4fa04f223e1e9b020d0985f8400fc616017da8
cherik <pourcheriki@gmail.com>
2021-10-06 11:57:23 +0330

    Add SemiTitle

---
+++ src/components/typography/body/SemiTitle.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { brandColors } from "../../../common/colors";
+
+export const SemiTitle = styled.div`
+    /* Body/SEMI-TITLE */
+    font-family: Red Hat Text;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+    text-transform: uppercase;
+    color: ${brandColors.deep[900]};
+`;
