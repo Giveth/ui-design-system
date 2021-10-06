0ceabc461865fd6fd4960f498c44c7f69c0f8d84
cherik <pourcheriki@gmail.com>
2021-10-06 11:56:29 +0330

    add subline

---
+++ src/components/typography/body/Subline.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { brandColors } from "../../../common/colors";
+
+export const Subline = styled.div`
+    /* Body/Subline */
+    font-family: Red Hat Text;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 150%;
+    color: ${brandColors.deep[900]};
+`;
