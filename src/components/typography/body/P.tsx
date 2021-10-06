3f018155869197f40cb288917b6f94c1ead0771c
cherik <pourcheriki@gmail.com>
2021-10-06 11:57:01 +0330

    Add P

---
+++ src/components/typography/body/P.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { brandColors } from "../../../common/colors";
+
+export const P = styled.div`
+    /* Body/P */
+    font-family: Red Hat Text;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+    color: ${brandColors.deep[900]};
+`;
