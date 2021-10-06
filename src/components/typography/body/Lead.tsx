150c4e2289db38558b652b8f87a67392d4513655
cherik <pourcheriki@gmail.com>
2021-10-06 11:57:28 +0330

    add Lead

---
+++ src/components/typography/body/Lead.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { brandColors } from "../../../common/colors";
+
+export const Lead = styled.div`
+    /* Body/Lead */
+    font-family: Red Hat Text;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 150%;
+    color: ${brandColors.deep[900]};
+`;
