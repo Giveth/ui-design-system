375bd1864ac5088a2727676124aa8dc5abe06a26
cherik <pourcheriki@gmail.com>
2021-10-06 11:56:17 +0330

    add Title

---
+++ src/components/typography/body/Title.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { brandColors } from "../../../common/colors";
+
+export const Title = styled.div`
+    /* Body/Title */
+    font-family: Red Hat Display;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 42px;
+    letter-spacing: -0.01em;
+    color: ${brandColors.deep[900]};
+`;
