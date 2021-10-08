fd74c883966bfac5ac2f2432728f9bcd695badb5
cherik <pourcheriki@gmail.com>
2021-10-09 00:09:10 +0330

    export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,23 @@
+export * from "./typography/GLink";
+export * from "./typography/Overline";
+export * from "./typography/body/B";
+export * from "./typography/body/Caption";
+export * from "./typography/body/Lead";
+export * from "./typography/body/P";
+export * from "./typography/body/SemiTitle";
+export * from "./typography/body/Subline";
+export * from "./typography/body/SublineBold";
+export * from "./typography/body/Title";
+
+export * from "./typography/displays/D1";
+export * from "./typography/displays/D2";
+export * from "./typography/displays/D3";
+
+export * from "./typography/headlines/H1";
+export * from "./typography/headlines/H2";
+export * from "./typography/headlines/H3";
+export * from "./typography/headlines/H4";
+export * from "./typography/headlines/H5";
+export * from "./typography/headlines/H6";
+
 export * from "./Button";
