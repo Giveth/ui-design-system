6f75fdd4d420146345ec1b11bcb3179c56083a2d
cherik <pourcheriki@gmail.com>
2021-10-11 10:09:52 +0330

    add container

---
+++ src/components/layout/Container.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    padding-right: 32px;
+    padding-left: 32px;
+    margin-right: auto;
+    margin-left: auto;
+
+    @media (min-width: 768px) {
+        max-width: 768px;
+    }
+
+    @media (min-width: 1024px) {
+        max-width: 1024px;
+    }
+
+    @media (min-width: 1280px) {
+        max-width: 1280px;
+    }
+`;
