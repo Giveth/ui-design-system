9966ffeb4c24fd0c491be22023817f73969e379f
cherik <pourcheriki@gmail.com>
2021-10-11 09:57:10 +0330

    add icons

---
+++ src/stories/icons/Arrow/ArrowLeft.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { IconArrowLeft } from '../../../components/icons/arrows/ArrowLeft';
+
+export default {
+  title: 'Example/Icons/Arrow',
+  component: IconArrowLeft,
+} as ComponentMeta<typeof IconArrowLeft>;
+
+const Template: ComponentStory<typeof IconArrowLeft> = (args) => <IconArrowLeft {...args}/>;
+
+export const ArrowLeft = Template.bind({});
+ArrowLeft.storyName = 'Left'
+ArrowLeft.args = {
+  color: 'white',
+  size: 32,
+};
---
+++ src/stories/icons/ArrowCircle/ArrowCircleLeft.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { IconArrowCircleLeft } from '../../../components/icons/arrowCircles/ArrowCircleLeft';
+
+export default {
+  title: 'Example/Icons/ArrowCircle',
+  component: IconArrowCircleLeft,
+} as ComponentMeta<typeof IconArrowCircleLeft>;
+
+const Template: ComponentStory<typeof IconArrowCircleLeft> = (args) => <IconArrowCircleLeft {...args}/>;
+
+export const ArrowCircleLeft = Template.bind({});
+ArrowCircleLeft.storyName = 'Left'
+ArrowCircleLeft.args = {
+  color: 'white',
+  size: 32,
+};
