935b397bd6cafe857b06ccc9994fd64631f39ea6
cherik <pourcheriki@gmail.com>
2021-10-14 14:54:59 +0330

    fix buttons onClick

--- src/components/buttons/type.ts
+++ src/components/buttons/type.ts
@@ -1,8 +1,10 @@
+import { MouseEventHandler } from "react";
+
 export interface IButtonContainerProps {
     buttonType?: 'primary' | 'secondary' | 'texty';
     size?: 'small' | 'medium' | 'large';
     disabled?: boolean;
-    onClick?: Function;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface IButtonProps extends IButtonContainerProps {
