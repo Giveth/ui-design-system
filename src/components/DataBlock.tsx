d7cdcca09a0d4f599c97e74711c3fa24f1593952
cherik <pourcheriki@gmail.com>
2021-10-14 15:06:49 +0330

    update styles

--- src/components/DataBlock.tsx
+++ src/components/DataBlock.tsx
@@ -12,25 +12,43 @@ interface IDataBlockProps {
     type?: 'primary' | 'secondary' | 'texty';
     ActionCb?: Function;
     caption?: string;
-}
-
-const DataBlockContainer = styled.div``;
-const TitleContainer = styled.div`
+  }
+  
+  const DataBlockContainer = styled.div``;
+  const TitleContainer = styled.div`
     display: flex;
-`;
-
-export const DataBlock:FC<IDataBlockProps> = ({ title, icon, subtitle, children, actionLabel, type, ActionCb, caption}) => {
+    align-items: center;
+  `;
+  
+  const DBSubtitle = styled(SemiTitle)`
+    color: ${brandColors.deep[100]}
+  `;
+  
+  const Body = styled(Lead)`
+    margin: 24px 0;
+  `;
+  
+  const DBButton = styled(Button)`
+    display: block;
+    width: 200px;
+  `;
+  
+  const DBCaption = styled(Caption)`
+    color: ${brandColors.deep[100]};
+    padding: 0 20px;
+  `;
+  
+  export const DataBlock: FC<IDataBlockProps> = ({ title, icon, subtitle, children, actionLabel, type, ActionCb, caption }) => {
     return (
-        <DataBlockContainer>
-            <TitleContainer>
-                <H3>{title}</H3>
-                {icon && icon}
-            </TitleContainer>
-            {subtitle && <SemiTitle color={brandColors.deep[100]}>{subtitle}</SemiTitle>}
-            <Lead> {children} </Lead>
-            {actionLabel && ActionCb && <Button label={actionLabel} buttonType={type} onClick={()=>{ActionCb()}}/>}
-            <Caption>{caption}</Caption>
-        </DataBlockContainer>
+      <DataBlockContainer>
+        <TitleContainer>
+          <H3>{title}</H3>
+          {icon && icon}
+        </TitleContainer>
+        {subtitle && <DBSubtitle color={brandColors.deep[100]}>{subtitle}</DBSubtitle>}
+        <Body> {children} </Body>
+        {actionLabel && ActionCb && <DBButton label={actionLabel} buttonType={type} onClick={ActionCb} />}
+        <DBCaption>{caption}</DBCaption>
+      </DataBlockContainer>
     );
-
-}
+  }
