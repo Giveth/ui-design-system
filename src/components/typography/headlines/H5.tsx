d3e58863d5f2f3dcf8a6209c3fce446117fd0301
cherik <pourcheriki@gmail.com>
2022-01-01 00:20:58 +0330

    change H5 to rem

--- src/components/typography/headlines/H5.tsx
+++ src/components/typography/headlines/H5.tsx
@@ -6,8 +6,8 @@ export const H5 = styled.h5<IHeadingProps>`
     font-family: TeX Gyre Adventor;
     font-style: normal;
     font-weight: ${props => props.weight || 500};
-    font-size: 25px;
-    line-height: 36px;
+    font-size: 1.56rem;
+    line-height: normal;
     letter-spacing: -0.005em;
     margin-top: 0;
     margin-bottom: 0;
