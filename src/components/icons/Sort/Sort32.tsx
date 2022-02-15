a91a4fe6f0deff6f642c4e489b19eb92a001b32d
Cherik <Pourcheriki@gmail.com>
2022-02-15 18:39:00 +0330

    feat: add sort icon

---
+++ src/components/icons/Sort/Sort32.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { IIconProps } from '../type';
+
+export const IconSort32: FC<IIconProps> = ({
+	size = 32,
+	color = 'currentColor',
+}) => (
+	<svg
+		width={size}
+		height={size}
+		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
+	>
+		<path
+			d='M13.3333 25.3333V6.66666L6.66666 13.3333M18.6667 6.66666V25.3333L25.3333 18.6667'
+			stroke={color}
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		/>
+	</svg>
+);
