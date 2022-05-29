46de89ad7df5aef111323e0ba7cacd03dfb57468
alireza-sharifpour <ascode94@gmail.com>
2022-05-29 14:09:04 +0430

    fix: Added Instagram icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -149,6 +149,7 @@ export * from './social-icons/Reddit';
 export * from './social-icons/Twitter';
 export * from './social-icons/Facebook';
 export * from './social-icons/Linkedin';
+export * from './social-icons/Instagram';
 export * from './social-icons/Wikipedia';
 export * from './social-icons/Youtube';
 export * from './social-icons/Docs/Docs';
