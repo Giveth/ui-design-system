b20518f585f1fb3ae670decec0047e82a2e5293d
Cherik <Pourcheriki@gmail.com>
2022-06-07 14:07:35 +0430

    fix: disable button color

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -17,23 +17,21 @@ const ButtonContainer = styled.button<IButtonContainerProps>`
 	gap: 4px;
 	white-space: nowrap;
 	${props => {
+		if (props.disabled) {
+			if (props.buttonType === 'texty') {
+				return `color: ${brandColors.giv[500]};background: unset};padding: 8px 24px;opacity: 0.5;`;
+			}
+			return `color: ${brandColors.giv[400]};background: ${brandColors.giv[500]};opacity: 0.5;`;
+		}
 		switch (props.buttonType) {
 			case 'primary':
-				return props.disabled
-					? `color: ${brandColors.giv[400]};background: ${brandColors.giv[500]};opacity: 0.5;`
-					: `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[500]};`;
+				return `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[500]};`;
 			case 'secondary':
-				return props.disabled
-					? `color: ${brandColors.giv[400]};background: ${brandColors.giv[500]};opacity: 0.5;`
-					: `color: ${neutralColors.gray[100]};background: ${brandColors.giv[500]};`;
+				return `color: ${neutralColors.gray[100]};background: ${brandColors.giv[500]};`;
 			case 'texty':
-				return props.disabled
-					? `color: ${brandColors.giv[500]};background: unset};padding: 8px 24px;opacity: 0.5;`
-					: `color: ${brandColors.deep[100]};background: unset};padding: 8px 24px;`;
+				return `color: ${brandColors.deep[100]};background: unset};padding: 8px 24px;`;
 			default:
-				return props.disabled
-					? `color: ${brandColors.giv[400]};background: ${brandColors.giv[500]};opacity: 0.5;`
-					: `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[500]};`;
+				return `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[500]};`;
 		}
 	}}
 	${props => (props.disabled ? '' : 'cursor: pointer;')}
