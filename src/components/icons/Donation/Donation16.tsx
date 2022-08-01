f6c0735ece8c04feb5e30fd945eb0c8da944aad7
Cherik <Pourcheriki@gmail.com>
2022-08-01 17:34:36 +0430

    add donation16

---
+++ src/components/icons/Donation/Donation16.tsx
@@ -0,0 +1,104 @@
+import React, { FC } from 'react';
+import { IIconProps } from '../type';
+
+export const IconDonation16: FC<IIconProps> = ({
+	size = 16,
+	color = 'currentColor',
+}) => (
+	<svg
+		width={size}
+		height={size}
+		viewBox='0 0 16 17'
+		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
+	>
+		<g clipPath='url(#clip0_11069_199804)'>
+			<path
+				d='M10.6056 4.59326V4.59326C12.9688 4.59326 14.8846 6.50907 14.8846 8.87233V13.1281C14.8846 14.785 13.5415 16.1281 11.8846 16.1281H4.11718C2.46033 16.1281 1.11719 14.785 1.11719 13.1281V9.33745C1.11719 6.71731 3.24123 4.59326 5.86137 4.59326V4.59326'
+				stroke={color}
+				strokeLinecap='round'
+			/>
+			<path
+				d='M1.61719 11.8257C1.61719 9.3404 3.63191 7.32568 6.11719 7.32568H9.88464C12.3699 7.32568 14.3846 9.3404 14.3846 11.8257V13.128C14.3846 14.5087 13.2653 15.628 11.8846 15.628H4.11718C2.73647 15.628 1.61719 14.5087 1.61719 13.128V11.8257Z'
+				stroke={color}
+			/>
+			<g clipPath='url(#clip1_11069_199804)'>
+				<path
+					d='M10.1394 3.10486C10.1394 2.02637 9.2644 1.15137 8.18591 1.15137C7.10742 1.15137 6.23242 2.02637 6.23242 3.10486C6.23242 4.18334 7.10742 5.05834 8.18591 5.05834C9.2644 5.05834 10.1394 4.18334 10.1394 3.10486Z'
+					fill='white'
+					stroke={color}
+					strokeWidth='0.75'
+					strokeMiterlimit='10'
+				/>
+				<g clipPath='url(#clip2_11069_199804)'>
+					<path
+						d='M8.1875 2.10986V4.09953'
+						stroke={color}
+						strokeWidth='0.4'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					/>
+					<path
+						d='M8.63936 2.47168H7.96107C7.87712 2.47168 7.79661 2.50503 7.73724 2.56439C7.67788 2.62375 7.64453 2.70427 7.64453 2.78822C7.64453 2.87217 7.67788 2.95268 7.73724 3.01204C7.79661 3.07141 7.87712 3.10475 7.96107 3.10475H8.41326C8.49722 3.10475 8.57773 3.1381 8.63709 3.19747C8.69645 3.25683 8.7298 3.33734 8.7298 3.42129C8.7298 3.50524 8.69645 3.58576 8.63709 3.64512C8.57773 3.70448 8.49722 3.73783 8.41326 3.73783H7.64453'
+						stroke={color}
+						strokeWidth='0.5'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					/>
+				</g>
+			</g>
+			<g clipPath='url(#clip3_11069_199804)'>
+				<path
+					d='M9.31278 9.24414C8.5579 9.24414 8.1858 9.98833 8.1858 9.98833C8.1858 9.98833 7.81371 9.24414 7.05883 9.24414C6.44534 9.24414 5.95952 9.7574 5.95324 10.3698C5.94045 11.6411 6.96173 12.5452 8.08115 13.305C8.11201 13.326 8.14848 13.3372 8.1858 13.3372C8.22313 13.3372 8.25959 13.326 8.29045 13.305C9.40976 12.5452 10.431 11.6411 10.4184 10.3698C10.4121 9.7574 9.92627 9.24414 9.31278 9.24414V9.24414Z'
+					stroke={color}
+					strokeWidth='0.75'
+					strokeLinecap='round'
+					strokeLinejoin='round'
+				/>
+			</g>
+			<line
+				x1='6.62617'
+				y1='5.78154'
+				x2='9.7471'
+				y2='5.78154'
+				stroke={color}
+				strokeWidth='0.6'
+				strokeLinecap='round'
+			/>
+		</g>
+		<defs>
+			<clipPath id='clip0_11069_199804'>
+				<rect
+					width='16'
+					height='16'
+					fill='white'
+					transform='translate(0 0.5)'
+				/>
+			</clipPath>
+			<clipPath id='clip1_11069_199804'>
+				<rect
+					width='5.2093'
+					height='5.2093'
+					fill='white'
+					transform='translate(5.58203 0.5)'
+				/>
+			</clipPath>
+			<clipPath id='clip2_11069_199804'>
+				<rect
+					width='2.17054'
+					height='2.17054'
+					fill='white'
+					transform='translate(7.10156 2.01953)'
+				/>
+			</clipPath>
+			<clipPath id='clip3_11069_199804'>
+				<rect
+					width='5.95349'
+					height='5.95349'
+					fill='white'
+					transform='translate(5.20898 8.31396)'
+				/>
+			</clipPath>
+		</defs>
+	</svg>
+);
