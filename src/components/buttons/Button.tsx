7a060cd5d914044955392824713b4ef7c01c5aba
Cherik <Pourcheriki@gmail.com>
2022-09-04 13:46:43 +0430

    update button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -10,7 +10,7 @@ const ButtonContainer = styled.button<IButtonContainerProps>`
 	border: 0 solid;
 	border-radius: 48px;
 	padding: ${props => (props.size === 'large' ? '24px' : '16px')} 24px;
-	transition: background 0.3s ease;
+	transition: background 0.3s ease, color 0.3s ease;
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -33,6 +33,10 @@ const ButtonContainer = styled.button<IButtonContainerProps>`
 				return `color: ${neutralColors.gray[100]};background: ${brandColors.giv[500]};`;
 			case 'texty':
 				return `color: ${brandColors.deep[100]};background: unset};padding: 8px 24px;`;
+			case 'texty-primary':
+				return `color: ${brandColors.pinky[500]};background: unset};padding: 8px 24px;`;
+			case 'texty-secondary':
+				return `color: ${brandColors.giv[500]};background: unset};padding: 8px 24px;`;
 			default:
 				return `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[500]};`;
 		}
@@ -48,6 +52,10 @@ const ButtonContainer = styled.button<IButtonContainerProps>`
 					return `color: ${neutralColors.gray[100]};background: ${brandColors.giv[700]};`;
 				case 'texty':
 					return `color: ${brandColors.deep[100]};background: ${brandColors.giv[700]}};`;
+				case 'texty-primary':
+					return `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[700]}};`;
+				case 'texty-secondary':
+					return `color: ${neutralColors.gray[100]};background: ${brandColors.giv[700]}};`;
 				default:
 					return `color: ${brandColors.deep[100]};background: ${brandColors.giv[700]}};`;
 			}
