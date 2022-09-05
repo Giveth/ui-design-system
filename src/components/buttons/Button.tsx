0d42d7a09ba0bbc044836d3c9cf800f1e8b26749
Cherik <Pourcheriki@gmail.com>
2022-09-05 13:05:39 +0430

    add css to Button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FC } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { brandColors, neutralColors } from '../../common/colors';
 import { ButtonText } from '../typography/ButtonText';
 import { Loader, LoadingContainer } from './common';
@@ -10,7 +10,7 @@ const ButtonContainer = styled.button<IButtonContainerProps>`
 	border: 0 solid;
 	border-radius: 48px;
 	padding: ${props => (props.size === 'large' ? '24px' : '16px')} 24px;
-	transition: background 0.3s ease, color 0.3s ease;
+	transition: background-color 0.3s ease, color 0.3s ease;
 	display: flex;
 	justify-content: center;
 	align-items: center;
@@ -19,26 +19,59 @@ const ButtonContainer = styled.button<IButtonContainerProps>`
 	${props => {
 		if (props.disabled) {
 			if (props.buttonType === 'texty') {
-				return `color: ${brandColors.giv[500]};background: unset};padding: 8px 24px;opacity: 0.5;`;
+				return css`
+					color: ${brandColors.giv[500]};
+					background-color: unset;
+					padding: 8px 24px;
+					opacity: 0.5;
+				`;
 			}
 			if (props.buttonType === 'primary') {
-				return `background: ${brandColors.pinky[300]};color: ${brandColors.pinky[200]};`;
+				return css`
+					background-color: ${brandColors.pinky[300]};
+					color: ${brandColors.pinky[200]};
+				`;
 			}
-			return `color: ${brandColors.giv[400]};background: ${brandColors.giv[500]};opacity: 0.5;`;
+			return css`
+				color: ${brandColors.giv[400]};
+				background-color: ${brandColors.giv[500]};
+				opacity: 0.5;
+			`;
 		}
 		switch (props.buttonType) {
 			case 'primary':
-				return `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[500]};`;
+				return css`
+					color: ${neutralColors.gray[100]};
+					background-color: ${brandColors.pinky[500]};
+				`;
 			case 'secondary':
-				return `color: ${neutralColors.gray[100]};background: ${brandColors.giv[500]};`;
+				return css`
+					color: ${neutralColors.gray[100]};
+					background-color: ${brandColors.giv[500]};
+				`;
 			case 'texty':
-				return `color: ${brandColors.deep[100]};background: unset};padding: 8px 24px;`;
+				return css`
+					color: ${brandColors.deep[100]};
+					background-color: unset;
+					padding: 8px 24px;
+				`;
 			case 'texty-primary':
-				return `color: ${brandColors.pinky[500]};background: unset};padding: 8px 24px;`;
+				return css`
+					color: ${brandColors.pinky[500]};
+					background-color: unset;
+					padding: 8px 24px;
+				`;
 			case 'texty-secondary':
-				return `color: ${brandColors.giv[500]};background: unset};padding: 8px 24px;`;
+				return css`
+					color: ${brandColors.giv[500]};
+					background-color: unset;
+					padding: 8px 24px;
+				`;
 			default:
-				return `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[500]};`;
+				return css`
+					color: ${neutralColors.gray[100]};
+					background-color: ${brandColors.pinky[500]};
+				`;
 		}
 	}}
 	${props => (props.disabled ? '' : 'cursor: pointer;')}
@@ -47,17 +80,35 @@ const ButtonContainer = styled.button<IButtonContainerProps>`
 			if (props.disabled) return '';
 			switch (props.buttonType) {
 				case 'primary':
-					return `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[600]};`;
+					return css`
+						color: ${neutralColors.gray[100]};
+						background-color: ${brandColors.pinky[600]};
+					`;
 				case 'secondary':
-					return `color: ${neutralColors.gray[100]};background: ${brandColors.giv[700]};`;
+					return css`
+						color: ${neutralColors.gray[100]};
+						background-color: ${brandColors.giv[700]};
+					`;
 				case 'texty':
-					return `color: ${brandColors.deep[100]};background: ${brandColors.giv[700]}};`;
+					return css`
+						color: ${brandColors.deep[100]};
+						background-color: ${brandColors.giv[700]};
+					`;
 				case 'texty-primary':
-					return `color: ${neutralColors.gray[100]};background: ${brandColors.pinky[700]}};`;
+					return css`
+						color: ${neutralColors.gray[100]};
+						background-color: ${brandColors.pinky[700]};
+					`;
 				case 'texty-secondary':
-					return `color: ${neutralColors.gray[100]};background: ${brandColors.giv[700]}};`;
+					return css`
+						color: ${neutralColors.gray[100]};
+						background-color: ${brandColors.giv[700]};
+					`;
 				default:
-					return `color: ${brandColors.deep[100]};background: ${brandColors.giv[700]}};`;
+					return css`
+						color: ${brandColors.deep[100]};
+						background-color: ${brandColors.giv[700]};
+					`;
 			}
 		}}
 	}
