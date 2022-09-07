d46a44e5f4fd59e0415b872b360735db2c2e3647
Ramin <r.ramazanpour@aut.ac.ir>
2022-09-07 17:57:57 +0430

    fix: add all sizes to profile icon

--- src/components/icons/Profile.tsx
+++ src/components/icons/Profile.tsx
@@ -7,8 +7,8 @@ export const IconProfile: FC<IIconProps> = ({
 }) => {
 	const size24 = (
 		<svg
-			width='24'
-			height='24'
+			width={size}
+			height={size}
 			viewBox='0 0 24 24'
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'
