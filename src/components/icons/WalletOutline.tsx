b9e2f856dba10b6f3ee0d980fed4fd89d07e0a04
Ramin <r.ramazanpour@aut.ac.ir>
2022-09-07 16:06:39 +0430

    fix: add all sizes to wallet outline

--- src/components/icons/WalletOutline.tsx
+++ src/components/icons/WalletOutline.tsx
@@ -7,8 +7,8 @@ export const IconWalletOutline: FC<IIconProps> = ({
 }) => {
 	const size16 = (
 		<svg
-			width='16'
-			height='16'
+			width={size}
+			height={size}
 			viewBox='0 0 16 16'
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'
