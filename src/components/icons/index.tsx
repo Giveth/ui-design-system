21a81bb7d133fa1c8b1cc4f66af252a9b177aa7c
Cherik <Pourcheriki@gmail.com>
2024-06-26 11:22:58 +0330

    fix: add IconBookmarkFilled24

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -67,6 +67,7 @@ export * from './Backward/Backward16';
 export * from './Backward/Backward24';
 export * from './Backward/Backward32';
 export * from './BookmarkFilled/BookmarkFilled16';
+export * from './BookmarkFilled/BookmarkFilled24';
 export * from './BulbOff/BulbOff';
 export * from './BulbOff/BulbOff16';
 export * from './BulbOff/BulbOff24';
